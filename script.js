b6f394834fc78c10356a1f29c85f9c8db5900551
dvasquez08 <dvasquez.422@gmail.com>
2023-10-17 12:48:59 -0600

    Added comments to all files

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+// Section where HTML elements are being selected from the DOM
+// The API key is free and won't charge me anything but just 
+// to follow best practices I didn't add it here but it works 
+// when I enter the key and test it for now, it's just a placeholder
+
 const apiKey = 'OPEN_WEATHER_API_KEY';
 const searchButton = document.getElementById('search-button');
 const searchInput = document.getElementById('search-bar');
@@ -6,12 +11,17 @@ const icon = document.getElementById('icon');
 const temperature = document.getElementById('temperature');
 const description = document.getElementById('description');
 
+// This is where the search-button element is configured. When it is clicked
+// It runs the function below to fetch the value entered, then calls the fetchWeather function
+
 searchButton.addEventListener(
     'click', () => {
         const query = searchInput.value;
         fetchWeatherData(query);
     }
 );
+
+// The function that fetches the data using the API Key, and displays an error message if the data can't be retrieved
 
 function fetchWeatherData(query) {
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${query}&units=metric&appid=${apiKey}`)
